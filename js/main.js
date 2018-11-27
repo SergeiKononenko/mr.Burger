f4885848ab26881fb0083fdf4a9aaa34f49f43f4
ds <knnnk.srg@gmail.com>
2018-11-27 20:34:27 +0300

    Добавил всплывающее меню

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+/*Всплывающее меню*/ 
+const popupMenu = document.querySelector('#popup-menu');
+const hamburgMenu = document.querySelector('#hamburger-menu');
+const popupMenuClose = document.querySelector('.popup-menu__close');
+const popupMenulink = document.querySelector('.popup-menu__list');
+const body = document.querySelector('body');
+
+
+ hamburgMenu.addEventListener('click', function(){   
+   popupMenu.style.display = 'flex' ;
+   body.style.overflow = 'hidden';
+
+})
+
+   popupMenuClose.addEventListener('click', function(){
+    popupMenu.style.display = 'none';
+    body.style.overflow = '';
+
+   })
+
+   
+   popupMenulink.addEventListener('click', function(){
+       popupMenu.style.display = 'none';
+       body.style.overflow = 'auto'; 
+       
+   });
